8757f8ffa39be78f43b41f9d6b81faa65654f781
Pinvheel <220522950+Pinvheel@users.noreply.github.com>
2025-07-16 21:02:22 -0400

    Removed placeholder code & added letter recognition logic

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
+char prompt ();
+
 int main() {
-    cout << "Hello World!\n";
+    string answer = "hello";
+    
+    if (find(answer.begin(), answer.end(), prompt()) == answer.end()) {
+        cout << "Wrong!\n";
+    }
+    else {
+        cout << "Right!\n";
+    }
     return 0;
+}
+
+void draw () {
+    // Draws the game state.
+    // Calls prompt.
+}
+
+char prompt () {
+    // Prompts user for a single character input, & returns input.
+    cout << "Input a letter in the word \"hello\"\n";
+    char input = cin.get();
+    return input;
 }
